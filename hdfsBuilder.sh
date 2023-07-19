9868d5fc2defaffa9548896f4f7167907669ffb5
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2023-07-19 14:32:56 +0300

    Added some execution instructions in the existing script

--- hdfsBuilder.sh
+++ hdfsBuilder.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+
+# DIRECTIONS FOR CLUSTER
+# 1) Build Locally using this script
+# 2) Perform remote to the cluster using this command copy scp -P 2222 hadoop-dist/target/hadoop-3.2.2.tar.gz  skalogerakis@139.91.183.36:/media/localdisk/skalogerakis/
+# 3) Before building make sure to delete all existing hadoop tmp files rm -r /tmp/hadoop*
+# 4) Untar file in the cluster 	tar -xvf ${HADOOP_VERSION}.tar.gz --directory ${MY_HOME} --one-top-level=hadoop --strip-components 1
+# 5) Use hdfs-standalone script (with -d) parameter to update the config files in all nodes
+# 6) Again make sure to delete all existing hadoop tmp files rm -r /tmp/hadoop*
+# 7) Format only in the NN
+
 # The default behavior is to ask for the directory where the project exists
 PROJECT_DIR="/home/skalogerakis/Documents/Workspace/hadoop/" #Kalogerakis LocalPC
 VERSION="3.2.2"
@@ -15,6 +25,7 @@ echo -e "Hadoop "$VERSION" compiled\n"
 
 
 # Untar compressed file into a temporary location
+# THIS IS THE COMPRESSED FILE WE WANT
 tar -xvf  ${PROJECT_DIR}hadoop-dist/target/hadoop-${VERSION}.tar.gz -C $HOME
 echo  "Extracting new version completed.\n\n"
 
@@ -27,3 +38,4 @@ echo  "Copy new version to previous config completed\n\n"
 
 # Remove temporary files
 rm -r $HOME/hadoop-${VERSION}/
+
